c37e7eebf4787b2c54d3bcfbeeb706040ab5add1
RustSpark <75129382+RustSpark@users.noreply.github.com>
2025-04-08 16:42:58 +0800

    qqq

--- aiodog/impl/core.py
+++ aiodog/impl/core.py
@@ -120,7 +120,7 @@ class Control:
                     )
                 )
             except core.StreamEmpty:
-                logger.error("")
+                pass
 
     def __call__(self, function):
         @wraps(function)
